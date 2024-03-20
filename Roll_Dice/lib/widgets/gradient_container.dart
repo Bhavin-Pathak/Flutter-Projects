21ead1167f1500ed1f23efde0055e89c3579439c
Bhavin Pathak <bupadhyay277@gmail.com>
2024-03-20 11:22:43 +0530

    Chore: Customs Widgets Added

---
+++ Roll_Dice/lib/widgets/gradient_container.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.purple,
+            Colors.blue,
+            Colors.red,
+            Colors.yellow,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Center(
+        child: Text("Hello Bhavin"),
+      ),
+    );
+  }
+}
