91a22304a426f8e61d7c14d38764cc6c29127948
Bhavin Pathak <bupadhyay277@gmail.com>
2024-03-24 14:29:57 +0530

    Chore: Assign init Methif In Main

--- Quiz/lib/main.dart
+++ Quiz/lib/main.dart
@@ -1,18 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:quiz/Screens/main_screen.dart';
+import 'package:quiz/Screens/question_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = const MainScreen(setActiveScreen);
+    super.initState();
+  }
+
+  void setActiveScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: true,
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              colors: [
+                Colors.purple,
+                Colors.cyan,
+              ],
+            ),
+          ),
+          child: activeScreen,
+        ),
       ),
     );
   }
