b0cfa85bdc1a1d93dfb5b0fdb6fc6d5d2bb7546c
Bhavin Pathak <bupadhyay277@gmail.com>
2024-04-02 19:58:25 +0530

    [chore]: Splash Screen Added

---
+++ Task_Manager/lib/screens/splash_screen.dart
@@ -0,0 +1,71 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        const Duration(seconds: 3),
+        () => Navigator.push(
+            context, MaterialPageRoute(builder: (_) => HomeScreen())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Container(
+            decoration: const BoxDecoration(color: Colors.white),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: Color.fromRGBO(250, 250, 250, 1),
+                          radius: 40.0,
+                          child: Icon(
+                            Icons.check_rounded,
+                            color: Color(0xFF18D191),
+                            size: 60.0,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 20.0),
+                        ),
+                        Text(
+                          "Task Manager",
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
